c3cbf7e0167e24733f1f99f2cfa28e72aa70b7bc
Austin WIley <wileybeer@gmail.com>
2020-05-11 17:54:18 -0600

    initail code for restaurant seach page

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,29 @@
-//main page component
+import React, { Component } from 'react';
+
+class RestaurantSearch extends Component {
+    //set initial state
+    state = {
+        data: []
+      }
+
+      //handle input change
+      //filter the data based on user inputs
+      //paginate data into pages of 10 items at a time
+      //once component did mound make API call and set state with response in alphabetical order
+      componentDidMount() {
+        console.log('Make Api call')
+      }
+
+  render() {
+    return (
+      <div>
+        <h1>Search Restaurants</h1>
+        {/* make seach input fields */}
+        {/* make pagination buttons */}
+        {/* make a table with the data from the API */}
+      </div>
+    );
+  }
+}
+
+export default RestaurantSearch;
--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ReastaurantSearch from './pages/index';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="App container" >
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +18,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+        <ReastaurantSearch />
+      </header> */}
+        <ReastaurantSearch />
     </div>
   );
 }

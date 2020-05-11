6fd7ee7fdad2deacb65dc868d68c8db13dfc8b99
Austin WIley <wileybeer@gmail.com>
2020-05-11 16:34:56 -0600

    adding to server.js

--- server.js
+++ server.js
@@ -1,45 +1,12 @@
-// const path = require('path');
-// const express = require('express');
-// const app = express();
-// const publicPath = path.join(__dirname, '..', 'public');
-// const port = process.env.PORT || 3000;
-// app.use(express.static(publicPath));
-// app.get('*', (req, res) => {
-//    res.sendFile(path.join(publicPath, 'index.html'));
-// });
-// app.listen(port, () => {
-//    console.log('Server is up!');
-// });
-
-const express = require("express");
-const path = require("path");
-
-const mongoose = require("mongoose");
-const PORT = process.env.PORT || 3001;
+const path = require('path');
+const express = require('express');
 const app = express();
-// const apiRoutes = require("./routes/apiRoutes");
-const routes = require("./routes");
-
-// Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-
-// Use apiRoutes
-// app.use("/api", apiRoutes);
-// app.use(routes);
-
-// Connect to the Mongo DB
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactbooklist");
-
-// Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 3000;
+app.use(express.static(publicPath));
+app.get('*', (req, res) => {
+   res.sendFile(path.join(publicPath, 'index.html'));
 });
-
-app.listen(PORT, function() {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+app.listen(port, () => {
+   console.log('Server is up!');
 });
